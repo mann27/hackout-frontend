7a6337c374de9cdf60ac0e3dd5a8d97c0fde80b9
Mann Shah <mannshah2712@gmail.com>
2019-10-05 20:01:14 +0530

    added particle.js

--- src/components/HomePage/index.js
+++ src/components/HomePage/index.js
@@ -1,10 +1,127 @@
 import React, { Component } from "react";
 
+import Particles from "react-particles-js";
+
 export default class HomePage extends Component {
   render() {
     return (
       <div>
-        <h1>HEllo from Home page!</h1>
+        <Particles
+          style={{
+            width: "100%",
+            height: "100%"
+          }}
+          params={{
+            particles: {
+              number: {
+                value: 80,
+                density: {
+                  enable: true,
+                  value_area: 1803.4120608655228
+                }
+              },
+              color: {
+                value: "#6C6A6A"
+              },
+              shape: {
+                type: "circle",
+                stroke: {
+                  width: 2,
+                  color: "#000000"
+                },
+                polygon: {
+                  nb_sides: 5
+                },
+                image: {
+                  src: "img/github.svg",
+                  width: 100,
+                  height: 100
+                }
+              },
+              opacity: {
+                value: 0.4008530152163807,
+                random: false,
+                anim: {
+                  enable: false,
+                  speed: 1,
+                  opacity_min: 0.1,
+                  sync: false
+                }
+              },
+              size: {
+                value: 1.5,
+                random: true,
+                anim: {
+                  enable: false,
+                  speed: 40,
+                  size_min: 0.1,
+                  sync: false
+                }
+              },
+              line_linked: {
+                enable: true,
+                distance: 300,
+                color: "#6C6A6A",
+                opacity: 0.4,
+                width: 2
+              },
+              move: {
+                enable: true,
+                speed: 12,
+                direction: "none",
+                random: false,
+                straight: false,
+                out_mode: "out",
+                bounce: false,
+                attract: {
+                  enable: false,
+                  rotateX: 600,
+                  rotateY: 1200
+                }
+              }
+            },
+            interactivity: {
+              detect_on: "canvas",
+              events: {
+                onhover: {
+                  enable: false,
+                  mode: "repulse"
+                },
+                onclick: {
+                  enable: true,
+                  mode: "push"
+                },
+                resize: true
+              },
+              modes: {
+                grab: {
+                  distance: 400,
+                  line_linked: {
+                    opacity: 1
+                  }
+                },
+                bubble: {
+                  distance: 400,
+                  size: 40,
+                  duration: 2,
+                  opacity: 8,
+                  speed: 3
+                },
+                repulse: {
+                  distance: 100,
+                  duration: 0.4
+                },
+                push: {
+                  particles_nb: 5
+                },
+                remove: {
+                  particles_nb: 2
+                }
+              }
+            },
+            retina_detect: true
+          }}
+        />
       </div>
     );
   }
